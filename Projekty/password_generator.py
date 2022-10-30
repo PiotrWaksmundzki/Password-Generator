b88c54d1dfdc8ce78bc11ac357cb18d6b1b4045c
Piotr Waksmundzki <piotr.1.waksmundzki@gmail.com>
2022-10-30 23:34:46 +0100

    addet passwor_generator.py file

---
+++ Projekty/password_generator.py
@@ -0,0 +1,51 @@
+# autor: Piotr Waksmundzki
+# pobranie wymaganych biblitek
+
+from tkinter import *
+import random, string
+import pyperclip
+
+#/utworzenie okna GUI
+
+root = Tk()
+root.geometry("400x400")
+root.resizable(0,0)
+root.title("Password Generator by Piotr Waksmundzki") # ribbon /wstązka
+
+#paramentry nagłówka
+
+head = Label(root, text = "Password Generator", font = 'arial 15 bold').pack()
+
+#określenie długości hasła
+
+pass_label = Label(root, text = "Password Lenght", font = 'arial 10 bold').pack()
+pass_len = IntVar()
+length = Spinbox(root, from_ = 8, to_ = 32, textvariable = pass_len, width = 15).pack()
+
+#funkcja generator hasła
+
+pass_str = StringVar()
+
+def Generator():
+    password = ""
+
+    for x in range(0,4):
+        password = password + random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation)
+        pass_str.set(password)
+    for y in range(pass_len.get() - 4):
+        password = password + random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation)
+    pass_str.set(password)
+    return password
+#przycisk
+
+Button(root, text = "Generate Password", command = Generator).pack(pady = 5)
+Entry(root, textvariable = pass_str).pack()
+
+#uncja kopiowanie hasła
+def Copy_password():
+    pyperclip.copy(pass_str.get())
+
+Button(root, text = 'Copy to clipboard', command = Copy_password).pack(pady = 5)
+
+#petla uruchamiająca program
+root.mainloop()
